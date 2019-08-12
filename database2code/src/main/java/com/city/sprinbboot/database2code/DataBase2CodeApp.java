51f66c81e0e261b2254581127953af6594e241c4
CITY <city_wangyi@163.com>
2019-08-12 15:42:16 +0800

    init

---
+++ database2code/src/main/java/com/city/sprinbboot/database2code/DataBase2CodeApp.java
@@ -0,0 +1,11 @@
+package com.city.sprinbboot.database2code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBase2CodeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DataBase2CodeApp.class);
+    }
+}
